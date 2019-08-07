4430ea27a0a343de2ec15e8f53dabf5bf091c1c5
Ian Be <ian84be@gmail.com>
2019-08-07 12:53:12 -0500

    upgrade to aug2019 project

--- index.js
+++ index.js
@@ -1,5 +1,1 @@
-const server = require('./server.js');
-
-server.listen(4000, () => {
-  console.log('\n* Server Running on http://localhost:4000 *\n');
-});
+// code away!
--- server.js
+++ server.js
@@ -1,32 +1,15 @@
-const express = require('express'); // importing a CommonJS module
-const helmet = require('helmet');
-
-const postRouter = require('./post/post-router.js');
-const userRouter = require('./user/user-router.js');
+const express = 'express';
 
 const server = express();
 
-
-// function upperCase(req,res,next) {
-//     let {name} = req.body;
-//     console.log(name);
-//     name = name.toUpperCase();
-//     console.log(name);
-//     console.log(req.body);
-//     next();
-// }
-
-
-server.use(express.json()); // built-in, no need to yarn add
-server.use(helmet()); // third party, need to npm install or yarn add it
-// server.use(upperCase);
-
-// routing
-server.use('/api/posts', postRouter);
-server.use('/api/users', userRouter);
-
-server.get('/', (req,res) => {
-    res.send(` <h1>Welcome to webapi-iii-challenge</h1> `)
+server.get('/', (req, res) => {
+  res.send(`<h2>Let's write some middleware!</h2>`)
 });
+
+//custom middleware
+
+function logger(req, res, next) {
+
+};
 
 module.exports = server;
